e254777b26b5420026b99abaa24fd38361a99ae7
ndaemy <yuyaebean@gmail.com>
2022-01-24 19:41:24 +0900

    ✨ 2장 완료

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import Counter from './components/Counter';
+
+const styles = StyleSheet.create({
+  full: {
+    flex: 1,
+  },
+});
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => setCount(count + 1);
+  const handleDecrease = () => setCount(count - 1);
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello React native!</Text>
-      </View>
+    <SafeAreaView style={styles.full}>
+      <Counter
+        count={count}
+        onIncrease={handleIncrease}
+        onDecrease={handleDecrease}
+      />
     </SafeAreaView>
   );
 };
