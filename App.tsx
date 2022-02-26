2cc6fd9c0b260b8707f5ea81b5675bd4202b3603
ndaemy <yuyaebean@gmail.com>
2022-02-26 12:40:18 +0900

    5.3.5 Sync header title with MaterialBottomTabNavigator

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  getFocusedRouteNameFromRoute,
+  NavigationContainer,
+  RouteProp,
+} from "@react-navigation/native";
 import MainScreen from "./screens/MainScreen";
 import DetailScreen from "./screens/DetailScreen";
 
@@ -13,11 +17,30 @@ export type RootStackParamList = {
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
+const getHeaderTitle = (route: RouteProp<RootStackParamList, "Main">) => {
+  const routeName = getFocusedRouteNameFromRoute(route) ?? "Home";
+  const nameMap = {
+    Home: "홈",
+    Search: "검색",
+    Notification: "알림",
+    Message: "메시지",
+  };
+
+  // @ts-expect-error
+  return nameMap[routeName];
+};
+
 const App: React.FC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Main" component={MainScreen} />
+        <Stack.Screen
+          name="Main"
+          component={MainScreen}
+          options={({ route }) => ({
+            title: getHeaderTitle(route),
+          })}
+        />
         <Stack.Screen name="Detail" component={DetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
