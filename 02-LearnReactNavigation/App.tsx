cc8c24aba807dfdc229bc3ff56fa11cb569a2758
ndaemy <yuyaebean@gmail.com>
2023-04-09 22:10:35 +0900

    feat: Complete 5.3.5

--- 02-LearnReactNavigation/App.tsx
+++ 02-LearnReactNavigation/App.tsx
@@ -1,6 +1,8 @@
 import {
+  getFocusedRouteNameFromRoute,
   NavigationContainer,
   NavigatorScreenParams,
+  RouteProp,
 } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -17,10 +19,27 @@ export type StackParamList = {
 const Stack = createStackNavigator<StackParamList>();
 
 const App = () => {
+  const getHeaderTitle = (route: RouteProp<StackParamList, 'Main'>) => {
+    const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home';
+    const nameMap: Record<string, string> = {
+      Home: '홈',
+      Search: '검색',
+      Notification: '알림',
+      Message: '메시지',
+    };
+    return nameMap[routeName];
+  };
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='Main' component={MainScreen} />
+        <Stack.Screen
+          name='Main'
+          component={MainScreen}
+          options={({ route }) => ({
+            title: getHeaderTitle(route),
+          })}
+        />
         <Stack.Screen name='Detail' component={DetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
