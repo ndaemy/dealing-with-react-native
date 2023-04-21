d75ef6172d4715633a65ed46f65490581d616d05
ndaemy <yuyaebean@gmail.com>
2023-04-21 23:47:16 +0900

    chore: Set eslint

---
+++ 04-PublicGallery/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  rules: {},
+};
